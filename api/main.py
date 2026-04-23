5c8bc18538d41709df4fc3e6e8671fee28c5481d
Patioco <toochio@live.com>
2026-04-23 07:12:39 -0400

    updated run testwith no redis in test

--- api/main.py
+++ api/main.py
@@ -31,9 +31,6 @@ def get_redis():
     raise Exception("Could not connect to Redis")
 
 
-r = get_redis()
-
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
@@ -42,6 +39,7 @@ def health():
 @app.post("/jobs")
 def create_job():
     try:
+        r = get_redis()
         job_id = str(uuid.uuid4())
         r.lpush(QUEUE_NAME, job_id)
         r.hset(f"job:{job_id}", mapping={"status": "queued"})
@@ -52,7 +50,11 @@ def create_job():
 
 @app.get("/jobs/{job_id}")
 def get_job(job_id: str):
-    status = r.hget(f"job:{job_id}", "status")
-    if not status:
-        raise HTTPException(status_code=404, detail="Job not found")
-    return {"job_id": job_id, "status": status}
+    try:
+        r = get_redis()
+        status = r.hget(f"job:{job_id}", "status")
+        if not status:
+            raise HTTPException(status_code=404, detail="Job not found")
+        return {"job_id": job_id, "status": status}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
